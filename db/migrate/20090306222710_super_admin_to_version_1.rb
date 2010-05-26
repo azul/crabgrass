977feeb4a4905558ff4826f24b1a965b27474986
Azul <azul@riseup.net>
2010-05-26 14:27:02 +0200

    CUSTOM: removed old super admin migration that does not exist anymore

--- db/migrate/20090306222710_super_admin_to_version_1.rb
+++
@@ -1,9 +0,0 @@
-class SuperAdminToVersion1 < ActiveRecord::Migration
-  def self.up
-    Engines.plugins["super_admin"].migrate(1)
-  end
-
-  def self.down
-    Engines.plugins["super_admin"].migrate(0)
-  end
-end
